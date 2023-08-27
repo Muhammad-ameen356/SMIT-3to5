cc33592a93fba9bab454001bba2b0970d2efd512
Muhammad Ameen <muhammadameenh1@gmail.com>
2023-08-27 17:11:05 +0500

    Class 12

---
+++ class 12/app.js
@@ -0,0 +1,96 @@
+// Chapter # 1 to 8
+
+// alert("Hello class 12");
+
+// var name = "Muhammad Ameen";
+// var name1;
+
+// name1 = "Muhammad Ameen"
+
+// name1 = "Muhammad Ameen"
+
+// alert(name)
+
+// console.log(name1)
+
+// Data Types
+
+// String ""
+
+// "Hello world"
+
+// Number
+
+// 1, 2, -1, 0, 0.65
+
+// Boolean
+
+// true || false
+
+var name1 = "Muhammad Ameen";
+var name2 = "Zahid";
+
+// alert(name1 + name2);
+
+// var num = 12;
+// var num2 = "Ameen";
+
+// console.log(num + 10);
+
+// console.log(num * num2);
+
+// Illegal Names
+
+// var name 1 = "ameen"
+// var name-1
+// var 1num
+// var typeof = "ameen"
+
+// Legal Names
+// var name_1;
+// var $ameen;
+// var num1
+
+// var myName = "ameen";
+
+// console.log(myname);
+
+// var num1 = 12;
+// var num2 = 12
+// console.log(num1 * num2 + 12);
+
+// var num1 = 14;
+// var num2 = 3;
+
+// var result = num1 % num2
+// console.log(result)
+
+// var result = 1 + 12 * 2;
+// var result = 12 / (2 * 2) - 23 + 12;
+
+// 12 / 4 - 23 + 12;
+// 3 - 23 + 12;
+// 3 - 11;
+
+// -- result +
+// ++ result +
+// -+ result -
+// +- result -
+
+// console.log(result);
+
+// BODMAS;
+// Bracket > Division > Multiply > Addition > Subtraction;
+
+
+
+// TODO Chapter # 1
+
+
+//? Q 1
+// alert("Hello world");
+
+
+//? Q 2
+
+// alert
