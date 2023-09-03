d3bcd9e09b365a98659205b456e44872dfd7469f
Muhammad Ameen <muhammadameenh1@gmail.com>
2023-09-03 17:11:10 +0500

    Class 13

---
+++ class 13/app.js
@@ -0,0 +1,107 @@
+// TODO: Chapter # 9 to 16
+
+// prompt("What is your name?");
+
+// var myName = prompt("Enter Name", "Ameen");
+// var age = +prompt("Enter Age");
+
+// alert(myName);
+
+// * == is only check values
+
+// * === is check value + data type
+
+// console.log(myName);
+// console.log(age);
+
+// var age = 20;
+
+// if (age === 20) {
+//   alert("you are allow");
+// } else if (age === 15) {
+//   alert("You are under age");
+// } else if (age === 12) {
+//   alert("you are 12 years old");
+// } else {
+//   alert("You are not allow");
+// }
+
+// e.g
+//  a is greater than b
+// a > b;
+
+// a is less than b
+// a < b
+
+// var age = +prompt("Enter Age");
+
+// var num1 = 20;
+// var num2 = 40;
+// if (num1 !== num2) {
+//   alert("abcd");
+// } else {
+//   alert("Not");
+// }
+
+// var age = +prompt("Enter Age");
+// var schoolClass = +prompt("Enter Class");
+
+// if (age < 20 || schoolClass > 10) {
+//   alert("You are allow");
+// } else {
+//   alert("Not allow");
+// }
+// var age = +prompt("Enter Age");
+// var schoolClass = +prompt("Enter Class");
+// var roll = +prompt("Enter Roll number");
+
+// if (age === 20) {
+//   //   if (roll === 1) {
+//   alert(" if");
+//   //   }
+// } else {
+//   if (age !== 20) {
+//     alert("Not run in else");
+//   }
+// }
+
+// if ((age < 20 && schoolClass > 10) || roll === 2121) {
+//   alert("You are allow");
+// } else {
+//   alert("Not allow");
+// }
+
+// var fruit1 = "Mango",
+//   fruit2 = "Apple",
+//   fruit3 = "Kela",
+//   fruit4 = "";
+
+var fruits = ["Mango", "Apple", "Kela", "Nashpati", "Kharboza"];
+
+var colors = ["Red", "Green", "Yellow", 3, true, false];
+
+// console.log(fruits[3]);
+// console.log(colors[2]);
+// console.log(fruits.length);
+// console.log(fruits[fruits.length / 2]);
+
+var fruits = ["Mango", "Apple", "Kela", "Nashpati", "Kharboza"];
+
+// ? Fort adding element in last
+// fruits.push("Orange");
+// fruits.push("abcd");
+
+// ? For removing last element
+// fruits.pop()
+
+// ? for removing first element
+// fruits.shift();
+
+// ? for adding alement in start
+// fruits.unshift("Orange");
+
+// fruits[2] = "Banana";
+
+// console.log(fruits, "Console last");
+
+document.write("Ameen Amen" + "<br>" + "Ameen" )
