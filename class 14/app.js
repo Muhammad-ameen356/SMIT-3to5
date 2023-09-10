0fef47c60e990627d28a13d2883485cbe8750d8a
Muhammad Ameen <muhammadameenh1@gmail.com>
2023-09-10 17:20:05 +0500

    Class 14

---
+++ class 14/app.js
@@ -0,0 +1,30 @@
+// var fruits = ["Mango", "Apple", "Kela", "Nashpati", "Kharboza"];
+// fruits.splice(2, 2);
+// console.log(fruits, "Original Array with splice");
+
+// var colors = ["Red", "Green", "Yellow"];
+// var sliceArr = colors.slice(1, 2);
+// console.log(colors, "Original Array with slice color");
+// console.log(sliceArr,"slice arr");
+
+var fruits = ["Mango", "Apple", "Kela", "Nashpati", "Kharboza"];
+
+// var fruitLength = fruits.length;
+
+// var isFound = false;
+
+// for (var i = 0; i < fruitLength; i++) {
+//   if (fruits[i] === "abcdfds    ") {
+//     isFound = true;
+//     console.log(i);
+//   }
+// }
+// if (isFound === false) {
+//   console.log("Not Found");
+// }
+
+// for (var i = 0; i < 5; i++) {
+//   for (var j = 0; j < 5; j++) {
+//     console.log(j);
+//   }
+// }
