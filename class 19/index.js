89c5d48082c011f91bdbb485a68f58374a26ae31
Muhammad Ameen <muhammadameenh1@gmail.com>
2023-10-15 17:05:05 +0500

    Class 19

---
+++ class 19/index.js
@@ -0,0 +1,70 @@
+// //! chapter # to 40
+
+// const classNo = 19;
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+
+// var result = sum(2, 4, 6);
+// var result1 = sum(2, 466, 6);
+// var result2 = sum(2, 44, 6);
+
+// console.log(result);
+
+// function sum() {
+//   var b = 12;
+//   var a = 10;
+// }
+// console.log(b);
+
+// sum();
+// console.log(a);
+
+// var a = 10;
+
+// function sum() {
+//   a = 12;
+// }
+
+// console.log(a);
+// sum();
+
+// let a = 10;
+
+// console.log(a);
+
+// a = 12;
+
+// a = 16;
+// console.log(a);
+
+// sum();
+// function sum() {
+//   console.log(12);
+// }
+// alert(a);
+// let a;
+
+// console.log(a * a);
+
+// var day = "march";
+// if (day === "jan") {
+//   console.log("January");
+// } else if (day === "feb") {
+//   console.log("Feburary");
+// } else if()
+// var day = "tue";
+
+// switch (day) {
+//   case "mon":
+//     console.log("Monday");
+//     break;
+//   case "tue":
+//     console.log("Tuesday");
+//     break;
+//   case "sun":
+//     console.log("Sunday");
+//     break;
+//   default:
+//     alert("No day Match!");
+// }
