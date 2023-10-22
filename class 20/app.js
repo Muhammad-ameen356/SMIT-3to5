f23c076cdbcfb36353a33f68f029cd2a4e8c390b
Muhammad Ameen <muhammadameenh1@gmail.com>
2023-10-22 16:57:02 +0500

    Class 20

---
+++ class 20/app.js
@@ -0,0 +1,49 @@
+// //! Class # 41 & 44
+
+//? While & Do while Loop
+
+//* Hoisting
+
+// console.log(x);
+// console.log(abc);
+
+// var x = 10;
+
+// function abc() {
+//   console.log("ameen Function");
+// }
+// var abc = "ameen";
+// console.log(abc());
+// var a = 101;
+// console.log(a);
+
+// function abc(val1, val2) {
+//   if (true) {
+//     let a = 10;
+//   }
+//   console.log(a);
+//   return val1 + val2;
+// }
+
+// console.log(a);
+// console.log(abc(10, 12));
+
+// for (let i = 0; i < 10; i++) {
+//   //   console.log(i);
+// }
+// let j = 0;
+// while (j < 10) {
+//   debugger;
+//   console.log(j);
+//   j++;
+// }
+
+// let i = 0;
+// do {
+//   i++;
+//   console.log(i);
+// } while (i > 10);
+
+// console.log("abc external"); // This is console
+// console.log("abc external");
+
