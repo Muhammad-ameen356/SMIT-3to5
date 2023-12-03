d57be06eaf9625e5c74aa0fd2df56ad875470714
Muhammad Ameen <muhammadameenh1@gmail.com>
2023-12-03 19:06:47 +0500

    Class 25

---
+++ Class 25/app.js
@@ -0,0 +1,61 @@
+// let abc = {
+
+// }
+
+
+// let personDetails = [
+//     {
+//         id: 12,
+//         name: "Muhammad Ameen",
+//         name: "Ameen"
+//     },
+//     {
+//         id: 12,
+//         name: "Muhammad Ameen"
+//     }
+// ]
+
+// for (let i = 0; i < personDetails.length; i++) {
+//     console.log(personDetails[i].name)
+//     personDetails[i].name
+// }
+
+
+// let student1 = {
+//     id: 1,
+//     name: "ameen",
+//     cnic: "4240111111111",
+//     mobileNo: "121212121",
+//     gender: "Male",
+//     salary: "150rupia",
+//     isActive: true
+// }
+
+
+// student1.name = "abcd"
+// // delete student1.name
+
+// console.log(student1)
+
+
+// let path = window.location.href
+// let path = window.location.assign("https://www.facebook.com/")
+// window.location.replace("https://www.facebook.com/")
+// console.log(path)
+// if (path.indexOf("admin") !== -1) {
+
+//     if (auth === false) {
+//         window.location.replace("http://www.me.com/lojack.html#guarantee");
+//     }
+//     console.log()
+// }
+
+function browserCtrl(e) {
+
+    e.preventDefault()
+
+    console.log("Form")
+
+    // window.location.reload()
+    // window.location.assign("https://www.facebook.com/")
+}
