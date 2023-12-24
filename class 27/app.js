fb7f3c6f930dac7823b3677b179a9460c5ef1679
Muhammad Ameen <muhammadameenh1@gmail.com>
2023-12-24 17:13:27 +0500

    Class 27

---
+++ class 27/app.js
@@ -0,0 +1,84 @@
+// console.log("Hello World");
+
+
+// x = 10
+
+// let x
+
+// const a = 10
+
+
+// if (true) {
+//     let abc = 10
+//     var x = 10
+// }
+
+
+// function abc() {
+//     var y = 10
+// }
+
+
+
+// (function () {
+//     var a = 10
+// }())
+
+// console.log(x)
+// console.log(y)
+
+// const arr = [1, 2, 3, 4, 5, 6, 7]
+// const even = [2, 4, 6, 8]
+
+// // arr.forEach(function (item) {
+// //     console.log(item)
+// // })
+
+// let oddeach = arr.forEach(item => item + 1)
+
+// let odd = even.map(item => item + 1)
+
+// let evenNumber = arr.filter((item) => item % 2 === 0)
+// console.log(evenNumber)
+// console.log(odd)
+// console.log(oddeach)
+
+// const abc = () => {
+
+// }
+
+
+// function add(x, y = 5) {
+//     return x + y
+// }
+
+// let val = add(1, 2)
+// console.log(val)
+
+// function add(a, b, c, ...x) {
+//     let val = 0
+//     x.forEach(item => val += item)
+//     return a + b + c + val
+// }
+
+// let val = add(1, 2, 3)
+// console.log(val)
+
+
+// const arr1 = [1, 2, 3, 4]
+// const arr2 = [5, 6, ...arr1]
+// console.log(arr2[3]++)
+
+
+// const arr3 = [...arr1]
+
+// arr3[1]++
+// console.log(arr3)
+// console.log(arr1)
+
+// if(arr3[1] === 3) {
+    
+// }
+
+// const myLuckyNumber = `My Lucky Number is ${arr3[1] === 3 ? 'Hurray' : 'Else'}`
+// console.log(myLuckyNumbe
