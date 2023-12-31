844ad0dc271d5c3b56f82b773ce352b7049d559f
Muhammad Ameen <muhammadameenh1@gmail.com>
2023-12-31 17:03:13 +0500

    Class 28

---
+++ class 28/app.js
@@ -0,0 +1,112 @@
+// let a = 0;
+
+// const result = a === 10 ? 10 : 5
+
+// console.log(result, "result")
+
+// const interpolation = `My favorite Number is ${result}`
+
+// console.log(interpolation)
+
+
+// const x = 10
+// const y = 12
+
+// const obj = {
+//     x,
+//     y
+// }
+// console.log(obj)
+// const quux = () => Math.random()
+
+// let obj = {
+//     foo: "bar",
+//     [`${quux()}`]: 42
+// };
+
+
+// console.log(obj)
+
+// const obj = {
+//     func() {
+//         console.log("Function From Obj Method")
+//     }
+// }
+// obj.func()
+
+
+
+// const arr = [1, 3, 4,65,65656,565]
+// const [a, , c, ...x] = arr
+
+// console.log(x)
+
+// console.log(`a is ${a}  c is ${c} c is ${x}`)
+
+
+// let arr = [1, 3, 4]
+// let [a, b, c] = arr
+
+// const re = [b, a] = [a, b]
+
+
+// console.log(`a is ${a}  b is ${b}`)
+
+// const obj = {
+//     name: "ameen",
+//     company: "saylaani",
+//     hobby: { isPlaying: true }
+// }
+
+// const { company, hobby: { isPlaying } } = obj
+// console.log(isPlaying, company)
+
+
+// var list = [1, 2,6]
+// var [x, y, z = 4] = list
+// console.log(`x is ${x}  y is ${y} z is ${z}`)
+
+
+// var obj = { a: 1, b:12 }
+// var { a, b = 34   } = obj
+// console.log(`a is ${a}  b is ${b} `)
+
+// var list = [1, 2, 6]
+// const func = ([a, b]) => {
+//     console.log(a, b)
+// }
+// func(list)
+
+// var obj = { a: 1, b: 12 }
+// const func = ({ b: number1, a: number2 }) => {
+//     console.log(number1, number2)
+// }
+// func(obj)
+
+
+// id:
+// username
+// email
+// isVerified: false
+// isActive:
+// Date
+
+// // Post Schema
+// id:
+// dateTime:
+// createdBy:
+// title
+// description
+// tags: arr
+// like
+// dislike
+// email:
+// name:
+
+
+
+// const arr = [
+//     { title: "adas", description: "adsadsadsa", }
+// ]
+
+
