5bb3a11a3f0f00813d5c2056c1f5fb8b1070525c
Muhammad Ameen <muhammadameenh1@gmail.com>
2024-02-18 16:13:44 +0500

    Class 34 || React Start

---
+++ new-app/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <h1>First Class </h1>
+    </>
+
+  );
+}
+
+export default App;
---
+++ new-app/src/component/Navbar/index.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+    <div>
+        <h1>My first Class Navbar</h1>
+    </div>
+    )
+}
+
+export default Navbar;
